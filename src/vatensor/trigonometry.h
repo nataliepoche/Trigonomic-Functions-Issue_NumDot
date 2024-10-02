1d7dcb95567beed9a9c96f819ebf95d8673b6ffa
Natalie Poche <nataliealena03@gmail.com>
2024-10-02 14:11:00 -0400

    Added asin, acos, atan, sinh, cosh, tanh, asinh, acosh, and atanh functions

--- src/vatensor/trigonometry.h
+++ src/vatensor/trigonometry.h
@@ -7,6 +7,23 @@ namespace va {
     VArray sin(const VArray& array);
     VArray cos(const VArray& array);
     VArray tan(const VArray& array);
+
+    // Added asin, acos, atan
+    VArray asin(const VArray& array);
+    VArray acos(const VArray& array);
+    VArray atan(const VArray& array);
+
+    // Added sinh, cosh, tanh
+    VArray sinh(const VArray& array);
+    VArray cosh(const VArray& array);
+    VArray tanh(const VArray& array);
+
+    // Added asinh, acosh, a tanh
+    VArray asinh(const VArray& array);
+    VArray acosh(const VArray& array);
+    VArray atanh(const VArray& array);
+
+
 }
 
 #endif //TRIGONOMETRY_H
